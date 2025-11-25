9150f086716b8ace932f89c4343b06c0622a0145
Paul Kilby <Kilby-P@ulster.ac.uk>
2025-11-25 15:35:12 +0000

    enemy

--- Assets/_Game/pauls stuff/code/health/Health.cs
+++ Assets/_Game/pauls stuff/code/health/Health.cs
@@ -1,18 +1,29 @@
+using System.Collections;
 using Unity.VisualScripting;
 using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Health : MonoBehaviour
 {
+    [Header("Health")]
 [SerializeField] private float startingHealth;
 public float currentHealth { get; private set;}
 private Animator anim;
 private bool dead;
 
+[Header("iFrames")]
+[SerializeField] private float iFramesDuration; 
+
+[SerializeField] private float numberOfFlashes; 
+private SpriteRenderer spriteRend; 
+
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>(); 
+        spriteRend = GetComponent<SpriteRenderer>();
     }
     public void TakeDamage(float _damage)
     {
@@ -21,7 +32,7 @@ private bool dead;
         if (currentHealth > 0)
         {
                 anim.SetTrigger("hurt");
-                //iframes
+                StartCoroutine(Invunerability()); 
         }
         else
         {
@@ -33,5 +44,22 @@ private bool dead;
             }
             
         }
+    }
+    public void AddHealth(float _value)
+    {
+        currentHealth =Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+
+    }
+    private IEnumerator Invunerability()
+    {
+        Physics2D.IgnoreLayerCollision(6,10, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1,0,0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white; 
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+         Physics2D.IgnoreLayerCollision(6,10, false);
     }
 }
---
+++ Assets/_Game/pauls stuff/code/meleeEnemy.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class meleeEnemy : MonoBehaviour
+{
+    [SerializeField] private float attackCoolDown;
+    
+    [SerializeField] private float range;
+    [SerializeField] private int damage;
+    [SerializeField] private BoxCollider2D BoxCollider; 
+    [SerializeField] private LayerMask Playerlayer; 
+    private float cooldownTimer = Mathf.Infinity;
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+        //Attack only when player in sight
+        if (PlayerInSight())
+        {
+             if (cooldownTimer >= attackCoolDown)
+        {
+            //Attack
+        }
+     } 
+    }
+
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(BoxCollider.bounds.center + transform.right * range * transform.localScale.x, new Vector3(BoxCollider.bounds.size.x * range, BoxCollider.bounds.size.y, BoxCollider.bounds.size.z), 0, Vector2.left, 0, Playerlayer);
+        return hit.collider !=null; 
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(BoxCollider.bounds.center + transform.right * range * transform.localScale.x, new Vector3(BoxCollider.bounds.size.x * range, BoxCollider.bounds.size.y, BoxCollider.bounds.size.z));
+    }
+}
