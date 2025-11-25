43ed10b3fdaff7c52464832d8f6b8b3147cdc91d
keadoeliking23 <keadoking23@gmail.com>
2025-11-25 19:31:17 +0000

    enemy near done

--- Assets/_Game/pauls stuff/code/meleeEnemy.cs
+++ Assets/_Game/pauls stuff/code/meleeEnemy.cs
@@ -11,6 +11,17 @@ public class meleeEnemy : MonoBehaviour
     [SerializeField] private LayerMask Playerlayer; 
     private float cooldownTimer = Mathf.Infinity;
 
+    private Animator anim;
+
+    private Health playerHealth;
+
+    private enemyPatrol enemypatrol; 
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        enemypatrol = GetComponentInParent<enemyPatrol>();
+    }
     private void Update()
     {
         cooldownTimer += Time.deltaTime;
@@ -19,19 +30,36 @@ public class meleeEnemy : MonoBehaviour
         {
              if (cooldownTimer >= attackCoolDown)
         {
-            //Attack
+                cooldownTimer = 0;
+                anim.SetTrigger("HandAttack");
         }
+             if (enemypatrol != null) 
+                enemypatrol.enabled = !PlayerInSight();
      } 
     }
 
     private bool PlayerInSight()
     {
         RaycastHit2D hit = Physics2D.BoxCast(BoxCollider.bounds.center + transform.right * range * transform.localScale.x * colldierDistance, new Vector3(BoxCollider.bounds.size.x * range, BoxCollider.bounds.size.y, BoxCollider.bounds.size.z), 0, Vector2.left, 0, Playerlayer);
+
+        if(hit.collider != null)
+            playerHealth = hit.transform.GetComponent<Health>();
+
+
         return hit.collider !=null; 
+
+
     }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(BoxCollider.bounds.center + transform.right * range * transform.localScale.x * colldierDistance, new Vector3(BoxCollider.bounds.size.x * range, BoxCollider.bounds.size.y, BoxCollider.bounds.size.z));
+    }
+    private void DamagePlayer()
+    {
+        // if player still in range damage her
+        if (PlayerInSight())
+            playerHealth.TakeDamage(damage);
+        
     }
 }
